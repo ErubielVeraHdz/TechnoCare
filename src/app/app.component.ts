d7745784a1f2ba4ccc9d371349ee3e00e45d44a3
leodan2702 <21690080@tecvalles.mx>
2024-10-29 22:24:27 -0600

    Cambio de agregación de carrusel, seccion nosotros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet,Routes,RouterLink,RouterLinkActive } from '@angular/router';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
-
+import { HomeComponent } from "./components/home/home.component";
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderMenuComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderMenuComponent, HomeComponent, NosotrosComponent, ServiciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
