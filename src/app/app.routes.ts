4d740a09099b255aff788f95eedd40eb02bda2e9
fcdelreal420 <elzeldero28@gmail.com>
2024-10-30 00:41:15 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,32 @@
 import { Routes } from '@angular/router';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { LoginComponent } from './components/login/login.component';
-import { FomularioDispositivoComponent } from './components/formulario-dispositivo/formulario-dispositivo.component';
+import { FormularioDispositivoComponent } from './components/formulario-dispositivo/formulario-dispositivo.component';
 import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ResumenComponent } from './components/resumen/resumen.component';
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosAdminComponent } from './components/servicios-admin/servicios-admin.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
 
 export const routes: Routes = [
+    {path:"", component:HomeComponent},
     {
-        path:"", component:HomeComponent
+        path:"header",
+        component:HeaderMenuComponent
     },
-
-    {
-        path:"nosotros",
-        component:NosotrosComponent
-    },
-
-    {
-        path:"servicios", 
-        component:ServiciosComponent
-    },
-
+     
+        {
+            path:"nosotros",
+            component:NosotrosComponent
+        },
+        {
+            path:"servicios",
+            component: ServiciosComponent
+        },
     {
         path:"registrarse",
         component:RegistrarseComponent
@@ -31,6 +37,30 @@ export const routes: Routes = [
     },
     {
         path:"registrar-dispositivo",
-        component:FomularioDispositivoComponent
-    }
+        component:FormularioDispositivoComponent
+    },
+    {
+        path:"admin",
+        component:AdminComponent,
+        children:[
+            {
+                path:"clientes",
+                component:ClientesComponent
+            },
+            {
+                path:"equipos",
+                component:EquiposComponent
+            },
+            {
+                path:"serviciosAdmin",
+                component: ServiciosAdminComponent
+            },
+            {
+                path:"resumen",
+                component:ResumenComponent
+            }
+        ]
+        }
+         
+        
 ];
